b124063f18b75d002fa6ce51681a664af6d8b7e7
oshio07 <skw398@gmail.com>
2022-11-30 13:13:46 +0900

    Initial Commit

---
+++ ScrollableSlidingTabs/ScrollableSlidingTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableSlidingTabsApp.swift
+//  ScrollableSlidingTabs
+//
+//  Created by Shigenari Oshio on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableSlidingTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
